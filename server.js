500e638cf9d88de0feb65546cecc31a6c42adf72
Sunny Ou <ousunny91@gmail.com>
2019-08-12 14:13:39 -0400

    Add user registration and user login

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const app = express();
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+// Routes
+const usersRoutes = require('./routes/api/users');
+const authRoutes = require('./routes/api/auth');
 
 // Connect Database
 connectDB();
@@ -12,5 +14,9 @@ connectDB();
 app.use(express.json({
   extended: false
 }));
+
+// Define routes
+app.use('/api/users', usersRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
