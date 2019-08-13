3d8464b43550cc1cf13044f48da787baf2f7ca99
Sunny Ou <ousunny91@gmail.com>
2019-08-13 17:37:57 -0400

    Create Profile model

---
+++ models/Profile.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  trips: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'trip'
+  }],
+  savedActivities: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'activity'
+  }]
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
