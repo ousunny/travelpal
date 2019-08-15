e295cbe5ef3757271680280247be3be01c796047
Sunny Ou <ousunny91@gmail.com>
2019-08-15 13:13:02 -0400

    Create Trip model

---
+++ models/Trip.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const TripSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }],
+  destination: {
+    type: String,
+    required: true
+  },
+  date: {
+    start: {
+      type: Date,
+      default: Date.now()
+    },
+    end: {
+      type: Date
+    }
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  information: {
+    type: String
+  }
+});
+
+module.exports = Trip = mongoose.model('trip', TripSchema);
