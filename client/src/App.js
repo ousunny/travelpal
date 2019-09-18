038656c9a1312abafc7cf188bbebcf3571355246
Sunny Ou <ousunny91@gmail.com>
2019-09-18 14:25:44 -0400

    Create file for login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './components/auth/Login';
+
 import './App.css';
 
-import BottomNavBar from './components/layout/BottomNavBar';
-
-const App = () => <BottomNavBar />;
+const App = () => (
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={Login} />
+    </Fragment>
+  </Router>
+);
 
 export default App;
