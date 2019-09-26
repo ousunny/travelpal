ecf88c70b85bcaa2f82c7c1139a5018d22f146f3
Sunny Ou <ousunny91@gmail.com>
2019-09-26 16:37:06 -0400

    Add route to check if user is logged in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,19 @@ const config = require('config');
 
 const User = require('../../models/User');
 
+// @route     GET api/auth
+// @desc      Check to see if user is logged in
+// @access    Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 // @route     POST api/auth
 // @desc      Authenticate user and get token
 // @access    Public
