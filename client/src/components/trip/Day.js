d6eb2ad80e322a5c8056ac23470db80df718eadb
Sunny Ou <ousunny91@gmail.com>
2019-10-07 17:54:10 -0400

    Layout for days and activities

--- client/src/components/trip/Day.js
+++ client/src/components/trip/Day.js
@@ -3,15 +3,30 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, List, Card, CardContent } from '@material-ui/core';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Paper,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core';
+import { ThumbUp } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
+  paper: {},
+  card: {
     marginBottom: theme.spacing(3),
     padding: theme.spacing(1)
   },
-  card: {
-    marginBottom: theme.spacing(1)
+  interested: {
+    marginRight: theme.spacing(1),
+    fontSize: '1.2rem'
+  },
+  icon: {
+    minWidth: '0'
   }
 }));
 
@@ -19,19 +34,28 @@ const Day = ({ day: { activities, date } }) => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.paper}>
-      <h5>{date}</h5>
-      <List>
-        {activities.length > 0 &&
-          activities.map(activity => (
-            <Card className={classes.card} key={activity._id}>
-              <CardContent>
-                <h6>{activity.title}</h6>
-              </CardContent>
-            </Card>
-          ))}
-      </List>
-    </Paper>
+    <Card className={classes.card}>
+      <CardHeader title={<Moment format="YYYY/MM/DD">{date}</Moment>} />
+      <Paper>
+        <List>
+          {activities.length > 0 &&
+            activities.map(activity => (
+              <ListItem key={activity._id}>
+                <ListItemText
+                  primary={activity.title}
+                  secondary={activity.description}
+                />
+                <div className={classes.interested}>
+                  {activity.interested.length}
+                </div>
+                <ListItemIcon className={classes.icon}>
+                  <ThumbUp />
+                </ListItemIcon>
+              </ListItem>
+            ))}
+        </List>
+      </Paper>
+    </Card>
   );
 };
 
