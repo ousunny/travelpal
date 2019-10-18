ec06fcbbfc17ed9e25be1e301044a60586c875b0
Sunny Ou <ousunny91@gmail.com>
2019-10-18 16:47:43 -0400

    Modify store to only use redux-logger if not production

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk, logger];
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+
+  middleware.push(logger);
+}
 
 const store = createStore(
   rootReducer,
