c02d23acbc258ea5bb9b050cfc6fdd73400db257
Sunny Ou <ousunny91@gmail.com>
2019-11-01 09:42:20 -0400

    Refactor to only show list if there are items to display

--- client/src/components/trip/Activities.js
+++ client/src/components/trip/Activities.js
@@ -8,16 +8,17 @@ import { Paper, List } from '@material-ui/core';
 const Activities = ({ tripId, activities }) => {
   return (
     <Paper>
-      <List>
-        {activities.length > 0 &&
-          activities.map(activity => (
+      {activities.length > 0 && (
+        <List>
+          {activities.map(activity => (
             <ActivityItem
               key={activity._id}
               tripId={tripId}
               activity={activity}
             />
           ))}
-      </List>
+        </List>
+      )}
     </Paper>
   );
 };
